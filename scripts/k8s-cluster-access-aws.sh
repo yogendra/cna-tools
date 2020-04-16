a60c18f77459d596ff7e013a8ddd3c9edbdc1a1d
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-04-17 02:40:23 +0800

    Fixed to handle updates

--- scripts/k8s-cluster-access-aws.sh
+++ scripts/k8s-cluster-access-aws.sh
@@ -1,43 +1,60 @@
 #!/usr/bin/env bash
-# USAGE: k8s-cluster-lb.sh cluster_name "master vms list" "subnet list" "vpc_id"
-# TIP: terraform subnets list 
-# "$(terraform output infrastructure_subnet_ids | tr -d ',' | tr  '\n' ' ')"
-# TIP: terraform vpc-id
-# "$(terraform output vpc_id)"
+# USAGE: k8s-cluster-lb.sh vpc_id cluster_name cluster_uuid [security_group_name]
+set -e 
 
-cluster_name=$1
-cluster_uuid=$2
-security_group=${3:-default}
+vpc_id=$1
+cluster_name=$2
+cluster_uuid=$3
+subnet_type=$4
+security_group=${5:-default}
+
+echo "VPC:$vpc_id, cluster:$cluster_name($cluster_uuid) subnet_type:$subnet_type secgrp:$security_group"
 
 
-master_vms=$(aws ec2 describe-instances --filters Name=tag-key,Values=kubernetes.io/cluster/service-instance_${cluster_uuid} --filter tag:instance_group,Values=master --query "Reservations[*].Instances[*].InstanceId" --output text)
+echo fetching subnets
+subnets=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${vpc_id}" "Name=tag:Name,Values=*${subnet_type}*" --query "Subnets[*].SubnetId" --output text)
 
-subnets="$(terraform output infrastructure_subnet_ids | tr -d ',' | tr  '\n' ' ')"
-vpc_id="$(terraform output vpc_id)"
-security_groups=$(aws ec2 describe-security-groups --group-name default --query "SecurityGroups[0].GroupId")
+echo fetching security group id
+security_group_id=$(aws ec2 describe-security-groups  --filters "Name=vpc-id,Values=${vpc_id}" --group-name ${security_group} --query "SecurityGroups[0].GroupId")
 
 lb_name="k8s-master-${cluster_name}"
 target_group="${lb_name}-tg-8443"
+echo "vpc: ${vpc_id}  $lb_name (subnets:$subnets) (tgtgrp:$target_group) (secgrpid:$security_group_id)"
 
-if ! aws elbv2 describe-load-balancers --name ${lb_name} &> /dev/null; then
-  aws elbv2 create-load-balancer --name ${lb_name} --type network --subnets ${subnets} --scheme internal
+lb_created=0
+if ! aws elbv2 describe-load-balancers --name ${lb_name}  &> /dev/null; then
+  echo lb creation
+  lb_created=1
+  aws elbv2 create-load-balancer --name ${lb_name} --type network  --scheme internal  --subnets ${subnets}
 fi
-lb_arn=$(aws elbv2 describe-load-balancers --names ${lb_name} --query "LoadBalancers[0].LoadBalancerArn")
+echo get lb arn
+lb_arn=$(aws elbv2 describe-load-balancers --names ${lb_name} --query "LoadBalancers[0].LoadBalancerArn"  --output text)
 
 if ! aws elbv2 describe-target-groups --name ${target_group} &> /dev/null; then
+  echo create target group
   aws elbv2 create-target-group --name ${target_group} --protocol TCP --port 8443 --vpc-id ${vpc_id}
-else
-target_group_arn=$(aws elbv2 describe-target-groups --name ${traget_group} --query "TargetGroups[0].TargetGrsoupArn")
+fi
+echo get target group arn
+target_group_arn=$(aws elbv2 describe-target-groups  --name ${traget_group}  --query "TargetGroups[0].TargetGroupArn" --output text)
+echo tg arn: $target_group_arn
 
 aws elbv2 describe-target-health --target-group-arn ${target_group_arn} --query 'TargetHealthDescriptions[*].Target.Id' --output text  | while read exiting_target; do
-  target_id="Id:${exiting_target}"
+  echo deregister exiting host $exiting_target
+  target_id="Id=${exiting_target}"
   aws elbv2 deregister-targets --target-group-arn ${target_group_arn} --targets $target_id
 done
 
-master_vms_ids=$(echo $master_vms | tr ' ' '\n' | sed 's/^i-/Id:i-/' | tr '\n' ' ')
-aws elbv2 register-targets --target-group-arn ${target_group_arn} --targets ${master_vm_ids}
+echo register all master vms
 
+aws ec2 describe-instances --filters "Name=vpc-id,Values=${vpc_id}" \
+  "Name=instance-state-name,Values=running" \
+  "Name=tag:job,Values=master" \
+  "Name=tag-key,Values=kubernetes.io/cluster/service-instance_${cluster_uuid}" \
+  --query "Reservations[*].Instances[*].InstanceId" \
+  --output text | while read vm ; do
+  echo $vm: register
+  aws elbv2 register-targets --target-group-arn ${target_group_arn} --targets Id=$vm
+done
 
-aws elbv2 create-listener --load-balancer-arn ${lb_arn} --protocol TCP --port 8443  \
---default-actions Type=forward,TargetGroupArn=${target_group_arn}
-
+echo Add Listner
+aws elbv2 create-listener --load-balancer-arn ${lb_arn} --protocol TCP --port 8443 --default-actions Type=forward,TargetGroupArn=${target_group_arn}
