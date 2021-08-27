bbcd29777cf0e1bbbf13e1851668ac528fee89e5
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2021-08-27 17:11:20 +0800

    Updated instructiosn to use local cert

--- scripts/generate-kubeconfig-from-sa.sh
+++ scripts/generate-kubeconfig-from-sa.sh
@@ -3,14 +3,14 @@ set -eo pipefail
 
 saname=${1?"Service Account name missing: generate-sa-kubeconfig.sh <service-account> <namespace>"}
 namespace=${2?"Namespace missing: generate-sa-kubeconfig.sh <service-account> <namespace>"}
+server=$(kubectl config  view --minify -o jsonpath="{.clusters[0].cluster.server}")
 
 context=$(kubectl config  current-context)
-cluster=$(kubectl config  view --flatten -o jsonpath="{.contexts[?(@.name == \"$context\")].context.cluster}")
-name=$(kubectl -n ${namespace} get sa ${saname} -o jsonpath='{.secrets[0].name}')
+cluster=$(kubectl config  view --minify -o jsonpath="{.clusters[0].name}")
 
-ca=$(kubectl -n ${namespace} get secret/$name -o jsonpath='{.data.ca\.crt}')
-token=$(kubectl -n ${namespace}  get secret/$name -o go-template='{{ .data.token | base64decode }}')
-server=$(kubectl config  view --flatten --minify -o jsonpath="{.clusters[?(@.name == \"$cluster\")].cluster.server}")
+secret_name=$(kubectl -n ${namespace} get sa ${saname} -o jsonpath='{.secrets[0].name}')
+ca=$(kubectl config  view --minify --raw -o jsonpath="{.clusters[0].cluster['certificate-authority-data']}")
+token=$(kubectl -n ${namespace}  get secret/${secret_name} -o go-template='{{ .data.token | base64decode }}')
 
 cat <<EOF
 apiVersion: v1
