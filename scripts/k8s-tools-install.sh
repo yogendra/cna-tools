ec7cbe858e676b80dba859e8065158d2a603d462
Yogi Rampuria <yrampuria@vmware.com>
2021-09-16 00:18:46 +0800

    fixed interpreter path

--- scripts/k8s-tools-install.sh
+++ scripts/k8s-tools-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bin/env bash
+#!/usr/bin/env bash
 
 curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o $HOME/bin/kubectl 
 chmod a+x $HOME/bin/kubectl
