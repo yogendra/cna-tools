a270a87e86d5fb07def314330929ed35d688f384
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-02-08 02:43:36 +0800

    Add set to build

--- scripts/k8s-tools-install.sh
+++ scripts/k8s-tools-install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+set -Eeuo pipefail
+
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
