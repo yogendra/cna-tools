a418174f49d4b1c9e0e88c08dbe47083a348d3d1
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-02-08 14:28:25 +0800

    add mkdir command

--- scripts/k8s-tools-install.sh
+++ scripts/k8s-tools-install.sh
@@ -2,6 +2,8 @@
 
 set -Eeuo pipefail
 
+mkdir -p $HOME/bin || true
+
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
